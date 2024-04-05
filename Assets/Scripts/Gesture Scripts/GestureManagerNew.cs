b931fa116767e598cb2b79750562af5c8db47e4c
KanonKongen4 <91434226+KanonKongen4@users.noreply.github.com>
2024-04-05 13:49:08 +0200

    Scene looks better now!
    - Changed sun rotation to match skybox
    - Water looks has shallowness now!
    - Changed the ambient light to be more "warm" and luminant

--- Assets/Scripts/Gesture Scripts/GestureManagerNew.cs
+++ Assets/Scripts/Gesture Scripts/GestureManagerNew.cs
@@ -11,6 +11,8 @@ public class GestureManagerNew : MonoBehaviour
     public bool pointingWithLeftHand, pointingWithRightHand;
     public XRHandPose pointingPose;
 
+    public XRHandPose pose1, pose2;
+
     public string PointingActionLeft, PointingActionRight, combinedGesturesString;
 
     public List<string> combinedGestureHistory;
@@ -25,7 +27,9 @@ public class GestureManagerNew : MonoBehaviour
     void Start()
     {
         AddPointingAtTargetDescription_LeftHand("giant wolf"); // Mock description for debugging;
-        AddPointingAtTargetDescription_RightHand("Tree (47)"); // Mock description for debugging;
+        //AddPointingAtTargetDescription_RightHand("Tree (47)"); // Mock description for debugging;'
+        DEBUG_AddGestureToLists();
+
         Debug.Log(PullCombinedGesturesString());
 
         gestureManagerDebuggingMenu = GetComponent<GestureManagerDebuggingMenu>();
@@ -33,6 +37,18 @@ public class GestureManagerNew : MonoBehaviour
         gestureManagerDebuggingMenu.ShowMenu();
     }
 
+    private void DEBUG_AddGestureToLists()
+    {
+        AddGestureToList_Right(pose2);
+        AddGestureToList_Left(pose1);
+    }
+    private void Update()
+    {
+     if(Input.GetKeyDown(KeyCode.J)) {
+            AddPointingAtTargetDescription_RightHand("A nice fish that jumped in the river water"); // Mock description for debugging;
+            RemoveGestureFromList_Left(pose1);
+        }   
+    }
     private void checkForPointingWithHands() // Checks if the current gesture is pointing for both the left and right hands;
     {
         if (GestureList_Left.Count > 0 && GestureList_Left[0] == pointingPose)
@@ -40,14 +56,18 @@ public class GestureManagerNew : MonoBehaviour
             pointingWithLeftHand = true;
         }
         else
+        {
             pointingWithLeftHand = false;
+                    }
 
         if (GestureList_Right.Count > 0 && GestureList_Right[0] == pointingPose)
         {
             pointingWithRightHand = true;
         }
         else
+        {
             pointingWithRightHand = false;
+        }
     }
     public void AddPointingAtTargetDescription_LeftHand(string interestPointDescription) // sets the current pointing string for left hand. 
     {
@@ -63,22 +83,22 @@ public class GestureManagerNew : MonoBehaviour
         string combinedString = "[User is ";
 
         //Left hand check
-        if (PointingActionLeft != null)
+        if (PointingActionLeft != "") // check if string is empty
             combinedString += PointingActionLeft; // eg. "pointing at XX with their left hand"
-        else if (GestureList_Left[0] != null)
-            combinedString += "doing a " + GestureList_Left[0] + " with their left hand";
+        else if (GestureList_Left.Count>0)
+            combinedString += "doing a " + GestureList_Left[0].name + " with their left hand";
 
         //Check if right hand is doing something and add an "and" between the two gesture strings.
-        if (PointingActionLeft != null || GestureList_Right[0] != null)
+        if (PointingActionRight != null || GestureList_Right[0] != null)
             combinedString += " and ";
 
         //Right hand check
-        if (PointingActionRight != null)
+        if (PointingActionRight != "")
             combinedString += PointingActionRight;
         else if (GestureList_Right[0] != null)
-            combinedString += "doing a " + GestureList_Right[0] + " with their right hand";
+            combinedString += "doing a " + GestureList_Right[0].name + " with their right hand";
 
-        combinedGesturesString += "]";
+        combinedString += "]";
 
         //Check if any gestures are being made at all, if not, send a clean string
         if (PointingActionLeft == null && GestureList_Left[0] == null && PointingActionRight == null && GestureList_Right[0] == null)
