b2824e6cac3441e13c538c6cf5fc76da96ad98d8
Tannez <92729005+Tannez@users.noreply.github.com>
2024-04-22 14:45:00 +0200

    Added Audio
    Scene now contains alot more sound in the environment. Sounds included are:
    - Birdsong in the forrest
    - Creaking in boat
    - Water travelling underneath the boat
    - Different animals connected to the farm (rooster, chicken, cow and sheep)
    - Music and people talking near a market
    - A dog howling in some forrest
    
    For the animals at the farm, a script has been made that randomly selects an audio clip between the different animals every 8 seconds and plays it while within a distance of 100 (unit) to the farm.

---
+++ Assets/AnimalSoundPlayer.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalSoundPlayer : MonoBehaviour
+{
+    [Header ("Game Objects")]
+    [SerializeField] private GameObject Boat;
+    [SerializeField] private GameObject Farm;
+    private float DistanceBoatToFarm;
+    float counterIndex = 0.0f;
+
+    [Header ("Audio")]
+    public AudioSource AnimalAudioSource;
+    public AudioClip[] AnimalSounds = new AudioClip [3];
+
+    // IEnumerator PlayChickenSound()
+    // {
+    //     AnimalAudioSource.PlayOneShot(AnimalSounds[0]);
+    //     yield return null;
+    // }
+
+    // IEnumerator PlayRoosterSound()
+    // {
+    //     AnimalAudioSource.PlayOneShot(AnimalSounds[1]);
+    //     yield return null;
+    // }
+
+    // IEnumerator PlayCowSound()
+    // {
+    //     AnimalAudioSource.PlayOneShot(AnimalSounds[2]);
+    //     yield return null;
+    // }
+
+    // IEnumerator PlaySheepSound()
+    // {
+    //     AnimalAudioSource.PlayOneShot(AnimalSounds[3]);
+    //     yield return null;
+    // }
+    
+    void Update()
+    {
+        DistanceBoatToFarm = Vector3.Distance(Boat.transform.position, Farm.transform.position);
+
+        if (DistanceBoatToFarm < 100)
+        {
+            counterIndex += Time.deltaTime;
+
+            while (counterIndex >= 8.0f)
+            {
+                AudioClip randomClip = AnimalSounds[Random.Range(0, AnimalSounds.Length)];
+                AnimalAudioSource.PlayOneShot(randomClip);
+                Debug.Log("Playing Animal Sound");
+                counterIndex = 0.0f;
+            }
+        }
+    }
+}
