295c636403f011fad2a717b2a7246d7e1dcff873
Tannez <92729005+Tannez@users.noreply.github.com>
2024-05-01 11:51:13 +0200

    Fading methods
    Created a folder with components needed for fading in the scene. The prefab FadeEnd should be placed on the main camera of the XR rig.  When inserted in scene, make sure the "FadeEnd" animator is put on the object if it for some reason is not set on. The script "FadeController" can be used to get the methods that trigger the fade animations. So, if you want the scene to fade when hitting the goal, a script can be made that get the "FadeOut" method from "FadeController", and triggers it when the boat hits the target.

---
+++ Assets/FadeOut - Components/FadeController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeController : MonoBehaviour
+{
+    //Script Inspiration -> https://www.youtube.com/watch?v=Oadq-IrOazg
+
+    //Get animator with fade animations and insert in inspector.
+    public Animator fadeAnimator;
+
+    //These are related to testing the methods of the script through number input.
+    [Tooltip("While active: Fades can be tested. Press 1 to run FadeOut. Press 2 to run FadeIn. Press 3 to run ShowEndText.")]
+    [SerializeField] private bool testFades = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Only allow to activate different functions with keyboard input if the testFades bool is true
+        if (testFades == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                FadeOut();
+                Debug.Log("Fading Out");
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                FadeIn();
+                Debug.Log("Fading In");
+            }
+
+            if (Input.GetKey(KeyCode.Alpha3))
+            {
+                ShowEndText();
+                Debug.Log("Showing End Text");
+            }
+        }
+    }
+
+    //Three different methods that can be called from anywhere (if needed) to start their respective animation.
+    public void FadeOut()
+    {
+        fadeAnimator.SetTrigger("FadeOut");
+    }
+
+    public void FadeIn()
+    {
+        fadeAnimator.SetTrigger("FadeIn");
+    }
+
+    public void ShowEndText()
+    {
+        fadeAnimator.SetTrigger("ShowEndText");
+    }
+}
