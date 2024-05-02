e184ab1ea0aa6f1ab54e75d4ff1c1730d4e4169f
RasmusKanne <78956812+RasmusKanne@users.noreply.github.com>
2024-05-02 13:48:26 +0200

    Last Points of Interests
    Changes to boat scene with last points of interests

--- Assets/Scripts/BoatRouteNavMesh.cs
+++ Assets/Scripts/BoatRouteNavMesh.cs
@@ -34,34 +34,114 @@ public class BoatRouteNavMesh : MonoBehaviour
 
             if (BoatToTargetDistance < 6 && TargetIndex < Targets.Count)
             {
+                // Generic Targets
+                
+                // End:
                 if (Targets[TargetIndex].CompareTag("Goal"))
                 {
                     fadeController.FadeOut();
                     fadeController.ShowEndText();
                     Debug.Log("Goal has been reached!");
                 }
+                // Speed Increase
                 else if (Targets[TargetIndex].CompareTag("Boost"))
                 {
                     BoatNavMeshAgent.speed += 1;
                     Debug.Log("Boat Speed has been boosted!");
                     TargetIndex += 1;
                 }
+                // Speed Decrease
                 else if (Targets[TargetIndex].CompareTag("Slow"))
                 {
                     BoatNavMeshAgent.speed -= 1;
                     Debug.Log("Boat Speed has been slowed!");
                     TargetIndex += 1;
                 }
+                
+                // Points of Interest
+
+                // Fishing Hut
+                else if (Targets[TargetIndex].CompareTag("FishingHut"))
+                {
+                    Debug.Log("Boat at Fishing Hut");
+                    currentPOI = PointsOfInterest[0];
+                    TargetIndex += 1;
+                }
+                // Rune Stone
+                else if (Targets[TargetIndex].CompareTag("RuneStone"))
+                {
+                    Debug.Log("Boat at Rune Stone");
+                    currentPOI = PointsOfInterest[1];
+                    TargetIndex += 1;
+                }
+                // Farmstead
+                else if (Targets[TargetIndex].CompareTag("Farmstead"))
+                {
+                    Debug.Log("Boat at Farmstead");
+                    currentPOI = PointsOfInterest[3];
+                    TargetIndex += 1;
+                }
+                // Lindholm Village
+                else if (Targets[TargetIndex].CompareTag("LindholmVillage"))
+                {
+                    Debug.Log("Boat at Lindholm Village");
+                    currentPOI = PointsOfInterest[4];
+                    TargetIndex += 1;
+                }
+                // Lindholm Hoeje
+                else if (Targets[TargetIndex].CompareTag("LindholmHoeje"))
+                {
+                    Debug.Log("Boat at Lindholm Hoeje");
+                    currentPOI = PointsOfInterest[5];
+                    TargetIndex += 1;
+                }
+                // Market Entrance
+                else if (Targets[TargetIndex].CompareTag("MarketEntrance"))
+                {
+                    Debug.Log("Boat at Market Entrance");
+                    currentPOI = PointsOfInterest[6];
+                    TargetIndex += 1;
+                }
+                // Fabric Trader
+                else if (Targets[TargetIndex].CompareTag("FabricTrader"))
+                {
+                    Debug.Log("Boat at Fabric Trader");
+                    currentPOI = PointsOfInterest[7];
+                    TargetIndex += 1;
+                }
+                // The Smith
+                else if (Targets[TargetIndex].CompareTag("Smith"))
+                {
+                    Debug.Log("Boat at Smith");
+                    currentPOI = PointsOfInterest[8];
+                    TargetIndex += 1;
+                }
+                // Boat Builder
+                else if (Targets[TargetIndex].CompareTag("BoatBuilder"))
+                {
+                    Debug.Log("Boat at Boat Builder");
+                    currentPOI = PointsOfInterest[9];
+                    TargetIndex += 1;
+                }
+                // Eriks Place
+                else if (Targets[TargetIndex].CompareTag("EriksPlace"))
+                {
+                    Debug.Log("Boat at Boat Builder");
+                    currentPOI = PointsOfInterest[10];
+                    TargetIndex += 1;
+                }
+
+                // Default
                 else
                 {
                     TargetIndex += 1;
                 }  
             }
 
-            if (currentPOI != PointsOfInterest[TargetIndex])
-            {
-                currentPOI = PointsOfInterest[TargetIndex];
-            }
+            //if (currentPOI != PointsOfInterest[TargetIndex])
+            //{
+            //    currentPOI = PointsOfInterest[TargetIndex];
+            //}
         }
     }
 }
